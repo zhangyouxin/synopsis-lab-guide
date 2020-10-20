3423f2dc2c9aed179b8c93cb556e6f5badedaac0
zhangyouxin <zhangyouxingg@126.com>
2020-10-20 03:31:21 -0700

    Initial commit.

---
+++ labs/lab6/csrc/uvm_dpi.d
@@ -0,0 +1,15 @@
+uvm_dpi.o: \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_dpi.cc \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_dpi.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/sv_vpi_user.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/vpi_user.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/veriuser.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/svdpi.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_common.c \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_regex.cc \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_hdl.c \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_hdl_vcs.c \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/vcsuser.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/mhpi_user.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/vhpi_user.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/dpi/uvm_svcmd_dpi.c
---
+++ solutions/lab6/csrc/uvm_verdi_dpi.d
@@ -0,0 +1,8 @@
+uvm_verdi_dpi.o: \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/etc/uvm-1.2/verdi/dpi/uvm_verdi_dpi.cpp \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/vpi_user.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/veriuser.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/sv_vpi_user.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/vpi_user.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/vcs_vpi_user.h \
+ /tools/synopsys/vcs-mx/vcs-mx/O-2018.09-SP2/include/sv_vpi_user.h
